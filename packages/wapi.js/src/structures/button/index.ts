7945d93d13c1dd387160c556de22a7248232a8a0
sarthakjdev <jsarthak448@gmail.com>
2023-10-25 23:27:04 +0530

    fix: linter fix
    Signed-off-by: sarthakjdev <jsarthak448@gmail.com>

--- packages/wapi.js/src/structures/button/index.ts
+++ packages/wapi.js/src/structures/button/index.ts
@@ -1,8 +1,7 @@
-import { type z } from 'zod'
 import { ButtonBuilderParamsSchemaType } from './schema'
 
 export class Button {
 	private static schema = ButtonBuilderParamsSchemaType
 
-	constructor(params: z.infer<typeof ButtonBuilderParamsSchemaType>) {}
+	constructor() {}
 }
