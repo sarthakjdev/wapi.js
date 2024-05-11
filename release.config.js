69ed6d1f15577533501d290cd8c9492c7c023d45
sarthakjdev <jsarthak448@gmail.com>
2024-05-11 20:45:58 +0530

    feat: add semantic release
    Signed-off-by: sarthakjdev <jsarthak448@gmail.com>

---
+++ release.config.js
@@ -0,0 +1,21 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/npm",
+      {
+        pkgRoot: "dist",
+      },
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["package.json"],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+  ],
+};
