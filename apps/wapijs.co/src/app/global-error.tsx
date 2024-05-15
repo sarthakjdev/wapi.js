7c0f25cacb6e566300e3c6f7c3e19fcf0f0d96bf
sarthakjdev <jsarthak448@gmail.com>
2024-05-15 20:55:27 +0530

    test: package error test
    Signed-off-by: sarthakjdev <jsarthak448@gmail.com>

---
+++ apps/wapijs.co/src/app/global-error.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+import Link from 'next/link'
+import { Button } from '@wapijs/ui'
+import { ArrowPathIcon, ChatBubbleBottomCenterIcon } from '@heroicons/react/24/solid'
+
+export default function GlobalError({
+	reset,
+	error
+}: {
+	error: Error & { digest?: string }
+	reset: () => void
+}) {
+	console.log({ error: error, stack: error.stack })
+	return (
+		<div className="flex h-[70vh] min-h-screen flex-col items-center justify-center gap-4">
+			<h2 className="mx-auto text-center text-xl text-gray-900">
+				Oops! Something went wrong{' '}
+			</h2>
+			<div className="flex gap-4">
+				<Button onClick={() => reset()} size={'medium'}>
+					<ArrowPathIcon className="h-6 w-6 text-white" /> Try again
+				</Button>
+				<Link title="contact-support" href={'/'} rel="">
+					<Button size={'medium'} variant={'outline'}>
+						<ChatBubbleBottomCenterIcon className="h-6 w-6 text-white" /> Contact
+						Support
+					</Button>
+				</Link>
+			</div>
+		</div>
+	)
+}
--- apps/wapijs.co/src/utils/api-extractor.ts
+++ apps/wapijs.co/src/utils/api-extractor.ts
@@ -189,9 +189,9 @@ export function parametersString(item: ApiDocumentedItem & ApiParameterListMixin
 }
 
 export function addPackageToModel(model: ApiModel, data: any) {
-	const nodeModulesPath = path.join(process.cwd(), 'node_modules');
-	const files = readdirSync(nodeModulesPath);
-	console.log("node_modules:", files);
+	const nodeModulesPath = path.join(process.cwd(), 'node_modules')
+	const files = readdirSync(nodeModulesPath)
+	console.log('node_modules:', files)
 	const tsdocConfiguration = new TSDocConfiguration()
 	const tsdocConfigFile = TSDocConfigFile.loadFromObject(data.metadata.tsdocConfig)
 	tsdocConfigFile.configureParser(tsdocConfiguration)
