7879227e2837d536b1be73b8f60ee7f8ec44fd9c
sarthakjdev <jsarthak448@gmail.com>
2024-05-15 20:27:16 +0530

    test: package error test
    Signed-off-by: sarthakjdev <jsarthak448@gmail.com>

--- apps/wapijs.co/src/app/docs/[version]/layout.tsx
+++ apps/wapijs.co/src/app/docs/[version]/layout.tsx
@@ -1,45 +1,45 @@
-import { Sidebar, type SidebarSectionItemData } from '~/components/nav/sidebar'
-import { ApiModel } from '@microsoft/api-extractor-model'
-import { resolveItemUri } from '~/reusable-function'
-import { notFound } from 'next/navigation'
-import { addPackageToModel, fetchDocumentationJsonDataFromSlug } from '~/utils/api-extractor'
+// import { Sidebar, type SidebarSectionItemData } from '~/components/nav/sidebar'
+// import { ApiModel } from '@microsoft/api-extractor-model'
+// import { resolveItemUri } from '~/reusable-function'
+// import { notFound } from 'next/navigation'
+// import { addPackageToModel, fetchDocumentationJsonDataFromSlug } from '~/utils/api-extractor'
 
 export const revalidate = 60 * 60 * 24 * 30
 export const dynamicParams = true
 
 export default async function VersionHomeLayout({
 	children,
-	params
+	// params
 }: Readonly<{
 	children: React.ReactNode
 	params: { version: string }
 }>) {
-	console.log({ params })
-	const documentationData = await fetchDocumentationJsonDataFromSlug(params.version)
-	const model = new ApiModel()
-	addPackageToModel(model, documentationData)
+	// console.log({ params })
+	// const documentationData = await fetchDocumentationJsonDataFromSlug(params.version)
+	// const model = new ApiModel()
+	// addPackageToModel(model, documentationData)
 
-	const pkg = model.tryGetPackageByName(`@wapijs/wapi.js`)
+	// const pkg = model.tryGetPackageByName(`@wapijs/wapi.js`)
 
-	if (!pkg) notFound()
+	// if (!pkg) notFound()
 
-	function transformMembersIntoSidebarData() {
-		if (!pkg) notFound()
-		const allMembers = pkg.members[0].members.map(
-			(item): SidebarSectionItemData => ({
-				href: resolveItemUri(item),
-				kind: item.kind,
-				name: item.displayName,
-				overloadIndex: 0
-			})
-		)
-		return allMembers
-	}
+	// function transformMembersIntoSidebarData() {
+	// 	if (!pkg) notFound()
+	// 	const allMembers = pkg.members[0].members.map(
+	// 		(item): SidebarSectionItemData => ({
+	// 			href: resolveItemUri(item),
+	// 			kind: item.kind,
+	// 			name: item.displayName,
+	// 			overloadIndex: 0
+	// 		})
+	// 	)
+	// 	return allMembers
+	// }
 
 	return (
 		<main className="mx-auto flex max-w-screen-2xl flex-col gap-4">
 			<aside className="fixed left-0 top-0 flex overflow-scroll p-10 pl-0">
-				<Sidebar members={transformMembersIntoSidebarData()} key={'sidebar'} />
+				{/* <Sidebar members={transformMembersIntoSidebarData()} key={'sidebar'} /> */}
 			</aside>
 
 			<section className=" p-10 pl-[26rem] ">{children}</section>
