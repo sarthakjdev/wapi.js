f8c2ebc811a3e4d578ce67ddfd022f740f554249
sarthakjdev <jsarthak448@gmail.com>
2024-05-18 14:44:09 +0530

    fix: add google tag manager for analytics [skip ci]
    Signed-off-by: sarthakjdev <jsarthak448@gmail.com>

--- apps/wapijs.co/src/app/layout.tsx
+++ apps/wapijs.co/src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import 'overlayscrollbars/overlayscrollbars.css'
 import { clsx } from 'clsx'
+import { GoogleTagManager } from '@next/third-parties/google'
+import { IS_PRODUCTION } from '~/constant'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,6 +19,11 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
+			{IS_PRODUCTION ? (
+				<>
+					<GoogleTagManager gtmId={'GTM-PSX33PK2'} />
+				</>
+			) : null}
 			<body className={clsx(inter.className, 'min-h-screen bg-[#121212]')}>{children}</body>
 		</html>
 	)
